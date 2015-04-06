a655252ea4b13272e98a0e119caac86675f20dd7
yazabara <17513901Github>
2015-04-06 20:04:49 +0300

    images loader was added

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,7 @@ require.config({
         jquery: '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min',
         less: '//cdnjs.cloudflare.com/ajax/libs/less.js/2.3.1/less.min',
         bootstrap: '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min',
+        imagesLoaded: '../vendor/imagesloaded.pkgd.min',
         underscore: '../vendor/underscore-min',
         domReady: '../vendor/domReady',
         skrollr: '../vendor/skrollr.min',
@@ -24,21 +25,25 @@ require.config({
     }
 });
 
-define('app', ['jquery', 'scrollReveal', 'less', 'skrollr', 'domReady'], function ($, scrollReveal, less, skrollr, domReady) {
+define('app', ['jquery', 'scrollReveal', 'less', 'skrollr', 'domReady', 'imagesLoaded'], function ($, scrollReveal, less, skrollr, domReady, imagesLoaded) {
 
     var config = {
-        reset:  true
+        reset: true
     };
 
-    window.sr= new scrollReveal(config);
-
+    window.sr = new scrollReveal(config);
 
     skrollr.init({
         forceHeight: false
     });
 
+    imagesLoaded($('#body'), function (instance) {
+        $(".loading").hide();
+        $('#body').removeClass("without-scroll");
+    });
+
     domReady(function () {
-        alert("dom ready");
+        console.log("dom ready");
     });
 
 });
