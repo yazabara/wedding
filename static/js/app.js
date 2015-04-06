eef9be66f6ef32a66904c60b2103adb4d4bf9049
yazabara <17513901Github>
2015-04-06 20:55:14 +0300

    small changes

--- static/js/app.js
+++ static/js/app.js
@@ -3,9 +3,7 @@ require.config({
         jquery: '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min',
         less: '//cdnjs.cloudflare.com/ajax/libs/less.js/2.3.1/less.min',
         bootstrap: '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min',
-        imagesLoaded: '../vendor/imagesloaded.pkgd.min',
         underscore: '../vendor/underscore-min',
-        domReady: '../vendor/domReady',
         skrollr: '../vendor/skrollr.min',
         scrollReveal: '../vendor/scrollReveal.min'
     },
@@ -39,6 +37,6 @@ define('app', ['jquery', 'scrollReveal', 'less', 'skrollr'], function ($, scroll
     setTimeout(function() {
         $(".loading").hide();
         $('#body').removeClass("without-scroll");
-    }, 2000)
+    }, 3000)
 
 });
